f445189c9a9383ec289c2b8e32469aa3377a4c95
APPSASSOC\anil.balne <anil.balne@appsassociates.com>
2022-08-31 09:23:31 +0530

    ec2 launch terraform file

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "APP" {
+  ami                         = "ami-0becdc92395f41727"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  key_name                    = "terraform"
+  vpc_security_group_ids      = [aws_security_group.mainsg.id]
+  tags = {
+    Name = "GIT Server"
+  }
+}
